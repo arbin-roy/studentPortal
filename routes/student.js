fad64cf9f670c8075dcbdefd7064994da88fb9a1
arbin-roy <arbin-roy@users.noreply.github.com>
2021-05-10 21:30:30 +0530

    videos API created

--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,8 @@ const Student = require("../models/students")
 const multer = require("multer")
 const upload = multer()
 
+const Video = require("../models/videos")
+
 router.post('/login', upload.none(), (req, res, next) => {
     Student.findOne({roll: req.body.roll, password: req.body.password})
     .then(stu => {
@@ -16,6 +18,22 @@ router.post('/login', upload.none(), (req, res, next) => {
         return res.status(200).json({
             "success": true,
             "data": stu
+        })
+    }).catch(next)
+})
+
+router.post('/videos', upload.none(), (req, res, next) => {
+    Video.find({ sem: req.body.sem, dept: req.body.dept })
+    .then(videos => {
+        if(videos){
+            return res.json({
+                "seccess": true,
+                "data": videos
+            })
+        }
+        return res.status(404).json({
+            "success": false,
+            "error": "No videos found for your department and semester"
         })
     }).catch(next)
 })
