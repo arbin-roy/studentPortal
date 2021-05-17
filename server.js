ff0e2ae03f76909ea9cbf37ff421e28fd0aad154
dreamer-rgb <soumabhabiswas55@gmail.com>
2021-05-17 23:01:17 +0530

    cors module added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = require("./config/keys").mongoURI;
 const path = require("path")
 const passport = require("passport")
 const bodyparser = require("body-parser")
+const cors = require('cors')
 
 app.use("/uploads", express.static(path.join(__dirname, './uploads')));
 
@@ -13,6 +14,18 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAn
 .then(() => {console.log("MongoDB Connected")}).catch(err => {
     console.log(err)
 })
+
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200, // For legacy browser support
+    methods: "GET, PUT,POST,DELETE"
+}
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.use(cors(corsOptions));
 
 passport.initialize()
 require("./config/passport-teacher")(passport)
